90b8885637e6178a0d1b55dd495bf1c8ac8f532a
thayanie <thayanie1999@gmail.com>
2016-08-11 15:18:34 -0400

    idk

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+var name = document.getElementById('name1');
+var pw = document.getElementById('pw');
+function store() {
+    localStorage.setItem('name1', name.value);
+    localStorage.setItem('pw', pw.value);
+}
+
+function check() {
+
+    // stored data from the register-form
+    var storedName = localStorage.getItem('name1');
+    var storedPw = localStorage.getItem('pw');
+
+    // entered data from the login-form
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+
+    // check if stored data from register-form is equal to data from login form
+   if(userName.value == storedName && userPw.value == storedPw) {
+        alert('You are loged in.');
+    }else {
+        alert('ERROR.');
+    }
+}
